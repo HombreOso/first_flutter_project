e2b169e8a8b14df89b35df0957c258c1629e55b4
Arthur Emig <arthur.erich.emig@gmail.com>
2023-09-15 10:45:31 +0200

    new folder for services

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../firebase_constants.dart';
-import './confirm_email_screen.dart';
+import '../screens/confirm_email_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
