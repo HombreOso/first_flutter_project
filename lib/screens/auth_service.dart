35a9753892fc29403df42bc5a13c8b32732105b4
Arthur Emig <arthur.erich.emig@gmail.com>
2023-09-15 10:09:01 +0200

    tutorial Mitch Koko google sign in

---
+++ lib/screens/auth_service.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: unused_local_variable, unused_field
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../firebase_constants.dart';
+import './confirm_email_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  // Google Sign In
+  signInWithGoogle() async {
+    // begin interactive sign in process
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+
+    // obtain auth details from request
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+
+    // create a new credential for user
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+
+    // sign in
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
